7c4bc6822698c4c7894e4de8a1f0f9d56c47d8b2
DylTrout <47491555+DylTrout@users.noreply.github.com>
2022-03-02 08:58:32 -0800

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,1 +1,5 @@
+::This is the script intended to add the custom pens
 
+mkdir "%HOMEPATH%\Documents\SMART Notebook"
+curl -LJO -o"%HOMEPATH%\Documents\SMART Notebook" https://github.com/DylTrout/SN-pen/raw/main/Modified/SMART%20Notebook/ToolsMenu.xml
+curl -LJO -o"%HOMEPATH%\Documents\SMART Notebook" https://github.com/DylTrout/SN-pen/raw/main/Modified/SMART%20Notebook/custom.xml
